a5169dd61993bbfb584e89681bd1e066c8cdab61
Mateusz Jakiel <matjakiel998@gmail.com>
2024-11-06 22:07:50 +0100

    Dodanie nowych plików lub aktualizacja projektu

--- GetTvShowTotalLength/Program.cs
+++ GetTvShowTotalLength/Program.cs
@@ -1,10 +1,94 @@
-﻿namespace GetTvShowTotalLength
+﻿using Newtonsoft.Json;
+using System.Dynamic;
+using System.Net.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace GetTvShowTotalLength
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Please provide a TV show name.");
+                Environment.Exit(1);
+            }
+            string title = args[0];
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage response = await client.GetAsync($"https://api.tvmaze.com/search/shows?q={title}"))
+                    {
+                        string ff = $"https://api.tvmaze.com/search/shows?q={title}";
+                        if (!response.IsSuccessStatusCode)
+                        {
+                            Console.WriteLine("Failed.");
+                            Environment.Exit(1);
+                        }
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        var showList = JsonConvert.DeserializeObject<List<dynamic>>(responseBody);
+
+
+                        if (showList == null || showList.Count == 0)
+                        {
+                            Environment.Exit(10);
+                        }
+
+                        var mostRecentShow = showList
+                          .Select(x => x.show)
+                          .OrderByDescending(show => DateTime.Parse((string)show.premiered))
+                          .FirstOrDefault();
+
+                        if (mostRecentShow == null)
+                        {
+                            Environment.Exit(10);
+                        }
+
+                        string showId = mostRecentShow.id;
+                        using (HttpResponseMessage episodesResponse = await client.GetAsync($"https://api.tvmaze.com/shows/{showId}?embed=episodes"))
+                        {
+                            if (!episodesResponse.IsSuccessStatusCode)
+                            {
+                                Console.WriteLine("Failed to serch episodes.");
+                                Environment.Exit(1);
+                            }
+
+                            string episodesResponseBody = await episodesResponse.Content.ReadAsStringAsync();
+                            var showData = JsonConvert.DeserializeObject<dynamic>(episodesResponseBody);
+                            var episodes = showData?._embedded?.episodes;
+
+                            if (episodes == null)
+                            {
+                                Console.WriteLine("No episodes found.");
+                                Environment.Exit(1);
+                            }
+
+                            int totalRuntime = 0;
+                            int? runtime = 0;
+                            foreach (var episode in episodes)
+                            {
+                                runtime = episode.runtime;
+                                if (runtime.HasValue)
+                                {
+                                    totalRuntime += runtime.Value;
+                                }
+                            }
+
+                            Console.WriteLine(totalRuntime);
+                        }
+                    }
+                }          
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine($"Bład - {ex.Message}");
+                Environment.Exit(1);
+            }
         }
+
     }
 }
